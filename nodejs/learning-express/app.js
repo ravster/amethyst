5dc55a1db46171a3d852b75680206f6f3b7df8d5
Ravi Desai <rd7190@gmail.com>
2012-11-16 21:22:10 -0500

    * Learned using '.use'.   Don't understand how this is different from '.get'.  Don't they both just do a callback?   API says that get() can call the value of a key.  Don't understand what its doing with callbacks, though.   use() defaults to '/'.  Unless you call it on something else.

--- nodejs/learning-express/app.js
+++ nodejs/learning-express/app.js
@@ -6,6 +6,17 @@ app.get('/hello.txt', function (req, res) {
     res.send('hello world :D:)');
 });
 
+// Defining another route here.  use() works on '/' by default.
+// next() just allows the system to finish the current callback and then continue on down the list of route handlers just in case there is an additional one that matches too.
+app.use(function(req,res,next){
+    console.log(req.method, req.url);
+    next();
+});
+
+app.use(function(req,res){
+    res.send('hello foo');
+});
+
 app.listen(8080);
 console.log('listening on port 8080.');
 
