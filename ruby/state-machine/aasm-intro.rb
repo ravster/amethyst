f5c96ffc80dc2bed1985e06adb384f4f2f7a3d00
Ravi Desai <rd7190@gmail.com>
2013-05-05 12:03:14 -0400

    Figuring out AASM using a script with nothing else to get in the way. * Note that you need to require the aasm module.  Use bundler to get that module. * Added "puts" statements so that I can see things when I run the script.

---
+++ ruby/state-machine/aasm-intro.rb
@@ -0,0 +1,34 @@
+# Taken straight from https://github.com/aasm/aasm
+require "aasm"
+
+class Job
+  include AASM
+
+  aasm do
+    state :sleeping, :initial => true
+    state :running
+    state :cleaning
+
+    event :run do
+      transitions :from => :sleeping, :to => :running
+    end
+
+    event :clean do
+      transitions :from => :running, :to => :cleaning
+    end
+
+    event :sleep do
+      transitions :from => [:running, :cleaning], :to => :sleeping
+    end
+  end
+end
+
+a = Job.new
+puts a.to_s
+puts a.sleeping?
+puts a.may_run?  # => true
+puts a.run
+puts a.running?  # => true
+puts a.sleeping? # => false
+puts a.may_run?  # => false
+puts a.run
