c3a781c10157e61b219d3e4ef35db07a38b13d9a
Ravi Desai <ravi@ravidesai.com>
2022-12-24 12:51:10 -0500

    Simple associative array of strings in C
    Random exercise I wanted to do, since I like the Hash datastructure in
    Ruby so very much.

---
+++ simple_assoc_array/ht.c
@@ -0,0 +1,62 @@
+/*
+gcc -o ht_string ht.c
+
+This implements a basic associative array using a simple char-array for both
+string keys and values.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct kv_string {
+  char key[100];
+  char val[100];
+};
+
+void kvs_print(struct kv_string* kvs, unsigned int count) {
+  struct kv_string kv;
+  printf("{\n");
+  for(int i = 0; i < count; i++) {
+    kv = kvs[i];
+    printf("\t%s: %s,\n", kv.key, kv.val);
+  }
+  printf("}\n");
+}
+
+void kv_set(struct kv_string* kv, char* key, char* val) {
+  strcpy(kv->key, key);
+  strcpy(kv->val, val);
+}
+
+int main(int argc, char* argv[]) {
+  if ((argc < 3) || (argc % 2 != 1)) {
+    printf("Usage: ./ht_string k1 v1 k2 v2\n");
+    exit(1);
+  }
+
+  struct kv_string kvs[16];
+  int count = 0;
+
+  for (int i = 1; i < argc; i+= 2) {
+    struct kv_string* kv = &kvs[count];
+
+    int len = strlen(argv[i]);
+    if (len > 99) {
+      printf("Too long:\n%s\n%s %d\n", argv[i], __FILE__, __LINE__);
+      exit(2);
+    }
+
+    len = strlen(argv[i+1]);
+    if (len > 99) {
+      printf("Too long:\n%s", argv[i+1]);
+      exit(2);
+    }
+
+    kv_set(kv, argv[i], argv[i+1]);
+
+    count++;
+  }
+
+  kvs_print(kvs, count);
+}
